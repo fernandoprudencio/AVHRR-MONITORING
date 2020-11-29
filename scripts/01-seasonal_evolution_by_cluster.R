a351a715d5ec8510c6f291a8561d71bd103b532a
fernando-desktop <fjprudenciop@gmail.com>
2020-11-28 19:04:46 -0500

    seasonal ndvi by cluster

---
+++ scripts/01-seasonal_evolution_by_cluster.R
@@ -0,0 +1,67 @@
+#' @title seasonal evolution by cluster
+#' @author Fernando Prudencio
+#'
+rm(list = ls())
+
+#' INSTALL PACKAGES
+pkg <- c(
+  "sf", "tidyverse"
+)
+
+sapply(
+  pkg,
+  function(x) {
+    is.there <- x %in% rownames(installed.packages())
+    if (is.there == FALSE) {
+      install.packages(x)
+    }
+  }
+)
+
+#' LOAD PACKAGES
+library(sf)
+library(tidyverse)
+library(Hmisc)
+
+#' LOAD NDVI VALUES
+load("data/rdata/ndvi_stations.RData")
+
+#' CONSTANTS
+k.cluster <- 10
+k.years.omit = c(2005, 2010, 2016)
+
+#' SELECT STATION BY CLUSTER
+sf.station <- st_read(
+  dsn = "data/vector/senamhi_weather_stations.gpkg",
+  layer = "weather_stations_HIST"
+) %>%
+  mutate(cod = as.character(cod)) %>%
+  dplyr::filter(clus == k.cluster)
+
+#' SELECT NDVI VALUES BY STATIONS
+ndvi <-
+  tibble(
+    ndvi = dplyr::select(df, sf.station$cod) %>%#sf.station$cod  "qc00000635"
+      apply(1, FUN = function(x) mean(x, na.rm = T))
+  ) %>%
+  mutate(
+    date = seq(as.Date("1981-06-01"), as.Date("2019-12-01"), by = "1 month"),
+    ndvi = ndvi * .0001
+  )
+
+normal.ndvi <-
+  dplyr::filter(ndvi, date >= "2019-01-01") %>%#str_sub(date, 1, 4) %nin% k.years.omit
+  group_by(date = str_sub(date, 6, 7)) %>%
+  summarise(ndvi = mean(ndvi, na.rm = T))
+
+plot(normal.ndvi$ndvi, type = "l")
+# barplot(normal.ndvi$ndvi, type = "l")
+
+
+dry.ndvi <-
+  dplyr::filter(ndvi, str_sub(date, 1, 4) %in% k.years.omit) %>%
+  group_by(date = str_sub(date, 6, 7)) %>%
+  summarise(ndvi = mean(ndvi, na.rm = T))
+
+dry.ndvi <-
+  dplyr::filter(ndvi, str_sub(date, 1, 4) == 2016)
